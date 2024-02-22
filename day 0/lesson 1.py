cd78b9eb1723e2e3afde2d4a03bc7d51f398d449
The1stRule <nikoloz0812@gmail.com>
2024-02-22 10:57:21 -0800

    first commit

---
+++ day 0/lesson 1.py
@@ -0,0 +1,25 @@
+name = "nika"
+surname = "kvaratskhelia"
+age= 16
+height = 167
+weight = 56
+hobby1 = "listening to music"
+hobby2 = "sport"
+hobby3 = "playing video games"
+
+
+
+
+
+# ობიექტი დაიფიქსირებულია. მიმდინარეობს ობიექტის იდენტიფიცირება. 
+#აქ შეიძლებოდა ფოტო ყოფილიყო)
+#.............     ............. 
+#ობიექტი იდენტიფიცირებულია:
+#ნიკა კვარაცხელია 16 წლის. ფიზიკური მონაცემები: სიმაღლე 167 სმ, წონა 56 კგ. ჰობი: მუსიკის სმენა, სპორტი , ვიდეო თამაშები ......
+
+
+print("Object detected. The object is being identified." , "The object is identificed:")
+print(name , surname , age , "years old. Physical characteristics: height" , height , "sm, weight" , weight , "kg. hobby:" , hobby1, "," , hobby2 , "," , hobby3 , "......"  )
+
+
+
