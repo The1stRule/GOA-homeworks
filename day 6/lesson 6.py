bc1d917a40409945d9710e0df76d0bc94fddd0ed
The1stRule <nikoloz0812@gmail.com>
2024-03-11 12:43:00 -0700

    sixth commit

---
+++ day 6/lesson 6.py
@@ -0,0 +1,92 @@
+
+ # 1)
+num1 = int(input("please enter first number: "))
+num2 = int(input("please enter second number: "))
+
+new_num1 = float(num1)
+
+print(type(new_num1))
+
+print(new_num1 == num2)
+
+ # 2)
+
+print(2 > 3)
+print(4 > 5)
+print(153 > 49)
+print(783 > 569)
+print(9239 > 9239)
+
+ # 3)
+
+print(77 >= 33)
+print(942 >= 3434)
+print(543 >= 543)
+print(315 >= 498)
+print(873 >= 321)
+
+ # 4)
+
+print(234 < 434)
+print(256 < 132)
+print(82 < 9)
+print(11 < 11)
+print(234 < 873)
+
+ # 5)
+
+print(854 <= 323)
+print(111 <= 111)
+print(287 <= 589)
+print(277 <= 8)
+print(233 <= 369)
+
+ # 6)
+
+print(558 == 558)
+print(239 == 153)
+print(797 == 213)
+print(425 == 321)
+print(901 == 901)
+
+ # 7)
+
+print(532 != 223)
+print(10 != 10)
+print(983 != 542)
+print(541 != 740)
+print(211 != 101)
+
+ # 8)
+
+num3 = int(input("please enter first number: "))
+num4 = int(input("please enter second number: "))
+
+print(num3 > num4)
+print(num3 < num4)
+print(num3 >= num4)
+print(num3 <= num4)
+print(num3 == num4)
+print(num3 != num4)
+
+ # 9)
+
+num5 = int(input("please enter a number: "))
+
+print(num5 > 10)
+print(num5 < 10)
+print(num5 >= 10)
+print(num5 <= 10)
+print(num5 == 10)
+print(num5 != 10)
+
+ # 10)
+
+num6 = int(input("please enter first number: "))
+num7 = float(input("please enter second number: "))
+
+print(type(num6))
+print(type(num7))
+
+print(num6 == num7)
+print(num6 != num7)
